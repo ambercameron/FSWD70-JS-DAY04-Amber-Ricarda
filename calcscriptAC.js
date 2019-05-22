338ef89bb00ce1f6fc92259d54d2a9e3d3f9780f
Amber Cameron <ambercameron285@gmail.com>
2019-05-22 15:16:23 +0200

    final calc edits

--- calcscriptAC.js
+++ calcscriptAC.js
@@ -4,23 +4,39 @@ for (let i=0; i<button.length; i++){
 button[i].addEventListener("click", display);
 }
 
-var string = (0)
+var string = ("")
 function display() {
 	string += (this.innerHTML);
-  document.getElementById("display").innerHTML=string
+  	document.getElementById("display1").innerHTML= string;
+  	var answer = (eval(string));
+	document.getElementById("display2").innerHTML= answer;
 }
+
+var button = document.getElementsByClassName("math");
+for (let i=0; i<button.length; i++){
+button[i].addEventListener("click", dismath);
+}
+
+var string = ("")
+function dismath() {
+	string += (this.innerHTML);
+  	document.getElementById("display1").innerHTML= string;
+}
+
 
 document.getElementById("clear").addEventListener("click", cleardisplay);
 
 function cleardisplay() {
-	string = (0)
-	document.getElementById("display").innerHTML=string
+	string = ("");
+	document.getElementById("display1").innerHTML=string;
+	document.getElementById("display2").innerHTML="";
 }
 
 document.getElementById("equal").addEventListener("click", calculate);
 
 function calculate() {
 	var answer = (eval(string));
-	document.getElementById("display").innerHTML= answer;
-	string = (0)
+	document.getElementById("display2").innerHTML= answer;
+	string = (answer)
+	document.getElementById("display1").innerHTML="";
 }
