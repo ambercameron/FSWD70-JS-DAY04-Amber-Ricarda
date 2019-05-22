01af950e641d567106c7b7ca9d34982fe9a9bfb6
Amber Cameron <ambercameron285@gmail.com>
2019-05-22 14:56:31 +0200

    simple calc file add

--- calcscriptAC.js
+++ calcscriptAC.js
@@ -1,8 +1,26 @@
-var button = document.getElementsByTagName("button");
+
+var button = document.getElementsByClassName("digit");
 for (let i=0; i<button.length; i++){
-button[i].addEventListener("click", showconsole);
+button[i].addEventListener("click", display);
 }
 
-function showconsole() {
-  console.log(this.innerHTML)
+var string = (0)
+function display() {
+	string += (this.innerHTML);
+  document.getElementById("display").innerHTML=string
+}
+
+document.getElementById("clear").addEventListener("click", cleardisplay);
+
+function cleardisplay() {
+	string = (0)
+	document.getElementById("display").innerHTML=string
+}
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+	var answer = (eval(string));
+	document.getElementById("display").innerHTML= answer;
+	string = (0)
 }
